b34776cd9ce11fcec1d78cee5313dc06977b0aba
Terry Urich <urichterry@gmail.com>
2017-10-11 11:16:18 -0700

    Initial

--- ManagersCrossing.java
+++ ManagersCrossing.java
@@ -1,1 +1,10 @@
+import java.util.Scanner;
+
+public static boolean currentBoat =true;
+
+public static boolean currentManagerA= true, currentManagerB=true, currentManagerC=true;
+public static boolean currentEngineerA=true, currentEngineerB=true, currentEngineerC=true;
+
+public static boolean nextManagerA=true, nextManagerB=true, nextManagerC=true;
+public static boolean nextEngineerA=true, nextEngineerB=true, nextEngineerc=true;
 
